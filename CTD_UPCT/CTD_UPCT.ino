5740cac5f6f3a6b954455eac9e953fc8a3d31fd7
Raniita <Raniita@users.noreply.github.com>
2019-11-07 14:31:45 +0100

    new version of arduino sketch

--- CTD_UPCT/CTD_UPCT.ino
+++ CTD_UPCT/CTD_UPCT.ino
@@ -10,13 +10,11 @@
 #include <Wire.h>
 #include <SPI.h>
 
-// Ethernet3 library for w5500
-#include <Ethernet3.h>
-#include <EthernetUdp3.h>
-//#include <Dhcp.h>
-//#include <Dns.h>
-//#include <EthernetClient.h>
-//#include <EthernetServer.h>
+// Ethernet Librarys (w5100 == Ethernet, w5500 == Ethernet, Ethernet3) 
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+//#include <Ethernet3.h>
+//#include <EthernetUdp3.h>
 
 //====================================================================
 //============================= Configurations =======================
@@ -50,19 +48,22 @@
 // Ethernet shield tiene la SD en el 4
 #define sd_pin 4
 
-// IP Configs
-// Cliente
+// Local configs
 #define hostname "upct_ctd"
 byte mac[6] = { 0x90, 0xA2, 0xDA, 0x2A, 0xB8, 0xCE };
-IPAddress ip(192, 168, 1, 177);
-IPAddress gw(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
 unsigned int local_port = 55055;
 
-// Server
-IPAddress ip_server(192, 168, 1, 5); // Server IP
+// IP Static config
+IPAddress ip(192, 168, 1, 177);
+IPAddress gw(192, 168, 1, 1);
+IPAddress my_dns(1, 1, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+// IP + Port Server
+IPAddress ip_server(192, 168, 1, 5);
 unsigned int server_port = 45045;
 
+// UDP configs
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
 String datReq;  //String for our data
 int packetSize; //Size of the packet
@@ -76,15 +77,20 @@ cyclopSensor cdom(cdom_id, cdom_read, cdom_x10, cdom_x100);
 cyclopSensor phy(phy_id, phy_read, phy_x10, phy_x100);
 cyclopSensor chl(chl_id, chl_read, chl_x10, chl_x100);
 
+String cdom_measure, cdom_mv, cdom_gain;
+String phy_measure, phy_mv, phy_gain;
+String chl_measure, chl_mv, chl_gain;
+String ms_pressure, ms_temp, ms_depth, ms_alt;
+String ts_temp;
+String message;
+
 MS5837 presion;
 TSYS01 temp;
 
 // Utilidades
-//EthernetClient client;
 EthernetUDP udp;
 
 void setup(){
-  // Configuracion puerto serie
   Serial.begin(BAUDRATE);
   Serial.println(F("Initializated serial connection"));
   delay(300);
@@ -98,142 +104,122 @@ void setup(){
   phy.setMaxPPB(phy_maxPPB);
   chl.setMaxPPB(chl_maxPPB);
 
-  Serial.println("CDOM Gain: x" + (String)cdom.getGain());
-  Serial.println("Phy Gain: x" + (String)phy.getGain());
-  Serial.println("CHL Gain: x" + (String)chl.getGain());
+  Serial.println("CDOM Gain: x" + (String) cdom.getGain());
+  Serial.println("Phy Gain: x" + (String) phy.getGain());
+  Serial.println("CHL Gain: x" + (String) chl.getGain());
 
   // Configuracion I2C sensors
   Wire.begin();
   temp.init();
   while (!presion.init()){
-    Serial.println("Presion sensor failed");
+    Serial.println(F("Presion sensor failed"));
     delay(2000);
   }
   presion.setModel(MS5837::MS5837_30BA);
   presion.setFluidDensity(1029); // kg/m^3 (freshwater, 1029 for seawater)
 
   // Configuracion Ethernet
-  //Ethernet.macAddress(mac);
-  Ethernet.setHostname(hostname);
-  Ethernet.begin(mac, ip, subnet, gw);
-  delay(3000);
-
-  // Check for Ethernet hardware present
-  /*if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    while (true) {
-      delay(1); // do nothing, no point running without Ethernet hardware
+  Serial.println(F("Initialize Ethernet with DHCP:")); 
+  // Static w5100
+  //Ethernet.begin(mac, ip, my_dns, gw, subnet); 
+  
+  // Static w5500 (Ethernet3)
+  //Ethernet.begin(mac, ip, subnet, gw);
+  
+  if(Ethernet.begin(mac) == 0){
+    Serial.println(F("Error on Ethernet. Not booting."));
+    while(true){
+      delay(1);
     }
   }
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
-  }
-  */
+  
+  delay(1500);
 
   udp.begin(local_port);
-
-  // Ethernet checks
-  Serial.println(Ethernet.macAddressReport()); 
-  Serial.println(Ethernet.phyState());
-  Serial.println(Ethernet.linkReport());
+  Serial.println("IP: " + (String) Ethernet.localIP() + " port: " + local_port);
+  Serial.println("IP Server: " + (String) ip_server + " port: " + server_port);
 
   Serial.println("");
   delay(2000);
 }
 
 void loop(){
-  // Lo primero que hacemos es comprobar si nuevo paquete
   packetSize = udp.parsePacket();
 
   if (packetSize > 0){
-    // Tenemos un nuevo mensaje
-    udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE); // Read the request
-    String datReq(packetBuffer);                    // Convert the buffer to datReq string
+    udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE); 
+    String datReq(packetBuffer);
 
-    // Aqui evaluamos en funcion de datReq
-    Serial.println(datReq);
-  } else {
-    send_data();
-    delay(2000);
-  }
+    // Mandamos lo recibido a la funcion
+    // Si es alguno de nuestros sensores, mandamos la medida
+    // Si no es, no hacemos nada
+    read_sensor(datReq);
+  } 
 }
 
-// Polling sensors + packet craft
-void send_data(){
-  String cdom_measure, cdom_mv, cdom_gain;
-  String phy_measure, phy_mv, phy_gain;
-  String chl_measure, chl_mv, chl_gain;
-  String ms_pressure, ms_temp, ms_depth, ms_alt;
-  String ts_temp;
-  String message;
-
-  // Mandamos sensor a sensor los datos
+void read_sensor(String mSensor){
   // Formato de los mensajes:
   // <type>;<gain>;<measure>;<mv> (cyclops)
   // <type>;<pressure>;<temp>;<depth>;<altitude>
   // <type>;<measure> (i2c sensors)
+  
+  if(mSensor == "cdom"){
+    cdom_measure = (String) cdom.sample();
+    cdom_mv = (String) cdom.measure();
+    cdom_gain = (String) cdom.getGain();
 
-  // CDOM Sensor
-  cdom_measure = (String)cdom.sample();
-  cdom_mv = (String)cdom.measure();
-  cdom_gain = (String)cdom.getGain();
+    message = "cdom;" + cdom_gain + ";" + cdom_measure + ";" + cdom_mv;
+    Serial.println("CDOM => x" + cdom_gain + " value: " + cdom_measure + " mV: " + cdom_mv);
+    send_message(message);
+  }
 
-  message = "cdom;" + cdom_gain + ";" + cdom_measure + ";" + cdom_mv;
+  if(mSensor == "phy"){
+    phy_measure = (String) phy.sample();
+    phy_mv = (String) phy.measure();
+    phy_gain = (String) phy.getGain();
 
-  Serial.println("CDOM => x" + cdom_gain + " value: " + cdom_measure + " mV: " + cdom_mv);
+    message = "phy;" + phy_gain + ";" + phy_measure + ";" + phy_mv;
+    Serial.println("PHY => x" + phy_gain + " value: " + phy_measure + " mV: " + phy_mv);
+    send_message(message);
+  }
 
-  send_message(message);
+  if(mSensor == "chl"){
+    chl_measure = (String) chl.sample();
+    chl_mv = (String) chl.measure();
+    chl_gain = (String) chl.getGain();
 
-  // Phyto Sensor
-  phy_measure = (String)phy.sample();
-  phy_mv = (String)phy.measure();
-  phy_gain = (String)phy.getGain();
+    message = "chl;" + chl_gain + ";" + chl_measure + ";" + chl_mv;
+    Serial.println("CHL => x" + chl_gain + " value: " + chl_measure + " mV: " + chl_mv);
+    send_message(message);
+  }
 
-  message = "phy;" + phy_gain + ";" + phy_measure + ";" + phy_mv;
+  if(mSensor == "ms5"){
+    presion.read();
+    ms_pressure = (String) presion.pressure();
+    ms_temp = (String) presion.temperature();
+    ms_depth = (String) presion.depth();
+    ms_alt = (String) presion.altitude();
 
-  Serial.println("Phy => x" + phy_gain + " value: " + phy_measure + " mV: " + phy_mv);
-
-  send_message(message);
-
-  // CHL Sensor
-  chl_measure = (String)chl.sample();
-  chl_mv = (String)chl.measure();
-  chl_gain = (String)chl.getGain();
-
-  message = "chl;" + chl_gain + ";" + chl_measure + ";" + chl_mv;
-
-  Serial.println("CHL => x" + chl_gain + " value: " + chl_measure + " mV: " + chl_mv);
-
-  send_message(message);
-
-  // MS5837 Sensor
-  presion.read();
-  ms_pressure = (String)presion.pressure();
-  ms_temp = (String)presion.temperature();
-  ms_depth = (String)presion.depth();
-  ms_alt = (String)presion.altitude();
-
-  message = "ms5;" + ms_pressure + ";" + ms_temp + ";" + ms_depth + ";" + ms_alt;
-
-  Serial.println("MS5837 => " + ms_pressure + " mbar, " + ms_temp + " C, " +
+    message = "ms5;" + ms_pressure + ";" + ms_temp + ";" + ms_depth + ";" + ms_alt;
+    Serial.println("MS5837 => " + ms_pressure + " mbar, " + ms_temp + " C, " +
                  ms_depth + " m, " + ms_alt + " m above sea");
+    send_message(message);
+  }
 
-  send_message(message);
+  if(mSensor == "temp"){
+    temp.read();
+    ts_temp = (String) temp.temperature();
 
-  // TSYS01 Sensor
-  temp.read();
-  ts_temp = (String)temp.temperature();
-
-  message = "temp;" + ts_temp;
-
-  Serial.println("TSYS01 => " + ts_temp + " C");
-
-  send_message(message);
+    message = "temp;" + ts_temp;
+    Serial.println("TSYS01 => " + ts_temp + " C");
+    send_message(message);
+  }
 }
 
-// Packet send
 void send_message(String msg){
   udp.beginPacket(ip_server, server_port);
   udp.print(msg);
   udp.endPacket();
+
+  delay(200);
 }
--- CTD_UPCT/receive_data.py
+++ CTD_UPCT/receive_data.py
@@ -1,10 +1,12 @@
 import socket, time
 
+# IP del arduino + puerto arduinos
 address = ('10.0.0.127', 55055)
 buffersize = 1024
 
 # Creates UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Dejamos a la escucha el puerto 45045
 sock.bind(("", 45045))
 
 while True:
