f88ede6ad5b1903185c2cccf0c0c4fe95116c479
Raniita <Raniita@users.noreply.github.com>
2019-11-09 13:33:54 +0100

    cleaning the repo

--- libraries/cyclopSensor/examples/phyto/phyto.ino
+++ libraries/cyclopSensor/examples/phyto/phyto.ino
@@ -1,24 +1,29 @@
 #include <cyclopSensor.h>
 
-// Definimos el sensor
-// En este caso utilizamos el sensor de phyto
-#define sensor_name_1 "PHY"
-#define sensor_id_1 1
-#define sensor_sn_1 21180134
-#define sensor_readPin_1 A1
-#define sensor_x10_1 8
-#define sensor_x100_1 9
-#define sensor_maxPPB_1 750
+#define phy_type "phy"
+#define phy_id "2"
+#define phy_read A1
+#define phy_x10 7
+#define phy_x100 6
+#define phy_maxPPB 750
 
-// Declaramos el sensor
-cyclopSensor phyto(sensor_id_1, sensor_sn_1, sensor_readPin_1, sensor_x10_1, sensor_x100_1, sensor_maxPPB_1);
+cyclopSensor phy(phy_id, phy_read, phy_x10, phy_x100);
+String phy_measure, phy_mv, phy_gain;
 
 void setup(){
 	Serial.begin(9600);
-	Serial.println("Sensor inizializado");
-	Serial.println(phyto.measure());
+	
+	phy.setMaxPPB(phy_maxPPB);
+	Serial.println("Sensor init.");
+	delay(200);
+	
+	Serial.println("Phy Gain: x" + (String) phy.getGain());
 }
 
 void loop(){
-	Serial.println("Valor medido: " + (String) phyto.measure());
+    phy_measure = (String) phy.sample();
+    phy_mv = (String) phy.measure();
+    phy_gain = (String) phy.getGain();
+    
+    Serial.println("PHY => x" + phy_gain + " value: " + phy_measure + " mV: " + phy_mv);
 }
